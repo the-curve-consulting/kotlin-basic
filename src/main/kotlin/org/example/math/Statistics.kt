a12db7566965459d1a8ea21bf81167f07366e250
James Ridgway <james@jamesridgway.co.uk>
2020-04-14 22:07:38 +0100

    Mocking and assertion library

--- src/main/kotlin/org/example/math/Statistics.kt
+++ src/main/kotlin/org/example/math/Statistics.kt
@@ -1,8 +1,6 @@
 package org.example.math
 
-class Statistics {
-    private val calculator = Calculator()
-
+class Statistics(private var calculator: Calculator) {
     fun average(values: Collection<Double>): Double {
         val total = calculator.sum(values)
         return calculator.divide(total, values.size.toDouble())
--- src/test/kotlin/org/example/math/StatisticsTest.kt
+++ src/test/kotlin/org/example/math/StatisticsTest.kt
@@ -1,20 +1,31 @@
 package org.example.math
 
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.BeforeEach
+import io.mockk.every
+import io.mockk.junit5.MockKExtension
+import io.mockk.mockk
+import io.mockk.verify
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 
+@ExtendWith(MockKExtension::class)
 class StatisticsTest {
 
     private lateinit var statistics: Statistics
+    private lateinit var calculator: Calculator
 
-    @BeforeEach
-    fun setupCalculator() {
-        statistics = Statistics()
-    }
     @Test
     fun `average returns 6 when 2,3,3,5,7,10 are provided`() {
-        assertEquals(5.0, statistics.average(listOf(2.0, 3.0, 3.0, 5.0, 7.0, 10.0)));
+        calculator = mockk<Calculator> {
+            every { sum(any()) }.returns(30.0)
+            every { divide(30.0, 6.0)}.returns(5.0)
+        };
+        statistics = Statistics(calculator);
+
+        assertThat(statistics.average(listOf(2.0, 3.0, 3.0, 5.0, 7.0, 10.0))).isEqualTo(5.0);
+        verify {
+            calculator.sum(listOf(2.0, 3.0, 3.0, 5.0, 7.0, 10.0))
+        }
     }
 
 }
